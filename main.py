7afd06689fc0dedf9260c87915b6980e7e5e7a7f
Sara <ironsara.fe@gmail.com>
2020-03-23 10:53:37 +0100

    modificato main.py

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ if n >= 0:
 	print("questo numero è positivo")
 else:
 	print("questo numero è negativo")
+
+m= (int(input("metti un altro numero")
+if m%2 == 0:
+    print("è pari")
+else:
+    print("è dispari")
